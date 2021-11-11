32f3868861924550793e5d2942e7d21660e6be8c
EddyBautista <bautista.j.eddy@gmail.com>
2021-11-11 09:30:05 -0600

    add 404 redirect

--- pages/404.js
+++ pages/404.js
@@ -1,5 +1,15 @@
 import Link from 'next/link'
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
 const NotFound = () => {
+    // redirect them to the homepage after 3 seconds.
+    const router = useRouter();
+    useEffect(() => {
+        setTimeout(() => {
+            router.push('/');
+        },3000)
+    }, [])  
     return ( 
         <div className="not-found">
             <h1>Oooooops......</h1>
