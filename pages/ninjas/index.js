d1b11603ba2c69c17f7b533e2d5bc3fe63076d72
EddyBautista <bautista.j.eddy@gmail.com>
2021-11-12 06:51:40 -0600

    add fetch method from jsonplaceholder

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,9 +1,25 @@
-const Ninjas = () => {
-    return ( 
-        <div> 
-        All Ninjas 
+// fetch method 
+export const getStaticProps = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+  return {
+    props: { ninjas: data },
+  };
+};
+
+const Ninjas = ({ ninjas }) => {
+  return (
+    <div>
+      <h1>Ninjas</h1>
+      {ninjas.map((ninja) => (
+        <div key={ninja.id}>
+          <a>
+            <h3>{ninja.name}</h3>
+          </a>
         </div>
-     );
-}
- 
+      ))}
+    </div>
+  );
+};
+
 export default Ninjas;
