4e5a2334c2a448be55fe3df96c24ad53ec6406a9
EddyBautista <bautista.j.eddy@gmail.com>
2021-11-12 06:53:51 -0600

    add styling to ninja list

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,3 +1,5 @@
+import styles from '../../styles/Ninjas.module.css'
+
 // fetch method 
 export const getStaticProps = async () => {
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
@@ -13,7 +15,7 @@ const Ninjas = ({ ninjas }) => {
       <h1>Ninjas</h1>
       {ninjas.map((ninja) => (
         <div key={ninja.id}>
-          <a>
+          <a className={styles.single}>
             <h3>{ninja.name}</h3>
           </a>
         </div>
